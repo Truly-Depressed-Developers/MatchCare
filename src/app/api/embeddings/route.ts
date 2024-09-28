678502607f9641a8b7d92a569d37ab0e32e371ff
Sosek1 <68021413+Sosek1@users.noreply.github.com>
2024-09-28 21:56:55 +0200

    api route for embedding search form input

---
+++ src/app/api/embeddings/route.ts
@@ -0,0 +1,31 @@
+import OpenAI from "openai";
+import { NextRequest, NextResponse } from "next/server";
+
+const openai = new OpenAI();
+
+const formInputEmbeddingHandler = async (
+  req: NextRequest,
+  res: NextResponse,
+) => {
+  if (req.method === "POST") {
+    try {
+      const { formInput } = await req.json();
+
+      const embedding = await openai.embeddings.create({
+        model: "text-embedding-3-small",
+        input: formInput,
+        encoding_format: "float",
+      });
+
+      return NextResponse.json({ embedding }, { status: 200 });
+    } catch (error) {
+      console.error("Error creating embedding:", error);
+      return NextResponse.json(
+        { error: "Internal Server Error" },
+        { status: 500 },
+      );
+    }
+  } else {
+    return NextResponse.json({ error: "Method not allowed" }, { status: 405 });
+  }
+};
